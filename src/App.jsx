8ced515b41526bfed6eee684ff9d6e0d775470ff
Prince abrahams <ccuser@CCs-MacBook-Air.local>
2025-01-08 15:26:05 +0200

    new code for change

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,22 @@ function App() {
   }
 
   return (
-    <div className='w-full h-screen duration-200 ' style={{backgroundColor: color}}>
-    <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-      <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-        <button
-        onClick={() => setColor('red')}
-        className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
-        style={{backgroundColor: 'red'}}
-        >Red</button>
-        <button
-        onClick={() => setColor('green')}
-        className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
-        style={{backgroundColor: 'green'}}
-        >Green</button>
-        
-        </div>
-    </div>
+    <div  style={{backgroundColor: color}}>
+      <a href='www.google.com'>
+
+      <h3>this is a new app</h3>
+      </a>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores adipisci perspiciatis quaerat fugit eaque doloremque deleniti obcaecati eveniet quod. Soluta quasi eius dolorum doloremque labore libero ea excepturi voluptas nulla?</p>
+      <button 
+      onClick={() => setColor('red')}
+      style={{backgroundColor: 'red'}}
+      >red</button>
+      <button
+      onClick={() => setColor('green')}
+      style={{backgroundColor: 'green'}}
+      >blue</button>
+
+    
   </div>
   )
 }
